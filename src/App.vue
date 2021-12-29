3928691a7ee0fd953f404f24cc30e8237b0960de
robin <529177108@qq.com>
2021-12-29 13:19:09 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<script>
+export default {
+  onLaunch: function () {
+    console.log('App Launch')
+  },
+  onShow: function () {
+    console.log('App Show')
+  },
+  onHide: function () {
+    console.log('App Hide')
+  }
+}
+</script>
+
+<style lang="scss">
+/*每个页面公共css */
+@import "uview-ui/index.scss";
+
+.test {
+  color: red;
+}
+.bottom-line {
+  position: fixed;
+  bottom: -5px;
+  left: 0;
+  width: 100vw;
+  height: 5px;
+  box-shadow: 0 -5px 5px #0000000d;
+		z-index: 999;
+}
+</style>
